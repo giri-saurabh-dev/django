a8b8bb236f79c322c316d2a3e4c0396b01153b1c
giri-saurabh-dev <saurabhgiri987@gmail.com>
2024-02-01 17:09:07 +0545

    models created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# About Model
+class About(models.Model):
+    short_description = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="about")
+    
+    class Meta:
+        verbose_name = "Abput Me"
+        verbose_name_plural = "About Me"
+        
+    def __str__(self):
+        return "About Me"
+    
+    
+    
+#Service Model
+class Service(models.Model):
+        name = models.CharField(max_length = 100, verbose_name = "Service name")
+        description = models.TextField(verbose_name = "About service")
+        
+        def __str__(self):
+            return self.name
+        
+#Recent Works
+class RecentWork(models.Model):
+    title = models.CharField(max_length = 100, verbose_name="Work title")
+    image = models.ImageField(upload_to="works")
+    
+    def __str__(self):
+        return self.title
+    
+# Client model
+
+class Client(models.Model):
+    name = models.CharField(max_length = 100, verbose_name="Client name")
+    description = models.TextField(verbose_name = "Client Say")
+    image = models.ImageField(upload_to="clients", default="default.png")
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
